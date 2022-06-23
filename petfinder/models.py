c69c12c403332bfb41ff3cc9306df6bc3ba475b6
carolyne <pcmaunda@gmail.com>
2022-06-23 10:52:25 +0300

    applied migrations for models classes

--- petfinder/models.py
+++ petfinder/models.py
@@ -1,14 +1,14 @@
 from distutils.command.upload import upload
 from django.db import models
 from django.forms import BooleanField, CharField, IntegerField
-from .models import Profile, Post,Comment
+from .models import *
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-  image= models.TextField(upload_to='image',blank=True)
+  image= models.ImageField(upload_to='image',blank=True)
   location = CharField(max_length=150)
   bio=models.TextField(max_length=240)
 
@@ -18,15 +18,14 @@ class Profile(models.Model):
 
 class Post(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
-  dog_name =CharField(max_length=150, blank=False)
-  image = models.CharField(max_length=500, blank=False)
+  dog_name =models.CharField(max_length=150, null=True)
+  image = models.CharField(max_length=500, blank=True)
   bio=models.TextField(max_length=300)
   color = models.CharField(max_length=150, blank=True)
   gender = models.CharField(max_length=150, blank=True)
   age = models.IntegerField(blank=True)
   breed = models.CharField(max_length=200, blank=True)
   size = models.IntegerField(blank=True)
-  available = BooleanField(default=False, blank=False)
   created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
